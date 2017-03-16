4a43bce53773084793169b25c24de026630ab514
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2017-03-16 13:32:00 -0700

    TMED.02-Solution-CustomizeLayout

--- app/src/main/java/com/example/android/classicalmusicquiz/QuizActivity.java
+++ app/src/main/java/com/example/android/classicalmusicquiz/QuizActivity.java
@@ -68,7 +68,6 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         // Initialize the player view.
         mPlayerView = (SimpleExoPlayerView) findViewById(R.id.playerView);
 
-        // TODO (1): Create a layout file called exo_playback_control_view to override the playback control layout.
 
         boolean isNewGame = !getIntent().hasExtra(REMAINING_SONGS_KEY);
 
